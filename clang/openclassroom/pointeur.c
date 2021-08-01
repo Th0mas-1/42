f4c227842e39e0fc8f8ec059262b5f8314834ffe
“ThomasW” <tom.0384@gmail.com>
2021-08-01 12:00:05 +0200

    pointeur updated

--- clang/openclassroom/pointeur.c
+++ clang/openclassroom/pointeur.c
@@ -4,11 +4,13 @@
 int main()
 {
     int age = 37;
-    int *mon_pointeur;
-    *mon_pointeur = &age;
+    int *mon_pointeur = &age;
+    //*mon_pointeur = &age;
 
     printf("La variable age vaut : %d\n", age);
     printf("L'adresse de la variable age est : %p\n", &age);
+    printf("L'adresse enregistrée comme valeur de mon pointeur est : %p\n", mon_pointeur);
+    printf("L'adresse de mon pointeur est : %p\n", &mon_pointeur);
 
     }
     
---
+++ clang/openclassroom/variables.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/* declaration des variables et constantes */
+int cote;
+int points;
+int moyenne;
+int mediane;
+const int RANG = 2500;
+
+/* assignation des variables et constantes */
+cote = -50;
+points = 80;
+moyenne = -75.9;
+mediane = 123.456;
+
+/* affichage des valeurs */
+int main() 
+{
+    printf("%d", cote);
+    return (0);
+}
+
